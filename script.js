d5e0683762b063e1a5b7bd7746cad7fd46996372
Anomalno <treskov.andrey@mail.ru>
2023-07-12 05:21:29 +0500

    fix

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-let nav
+let W1 = document.querySelector(".nav-block-button");
+let W2 = document.querySelector(".nav-block-2");
+let W11 = getComputedStyle(W1);
+let W22 = getComputedStyle(W2);
+let Wq = parseFloat(W22.paddingLeft);
+console.log(Wq)
+let Ww = parseFloat(W11.width);
+console.log(Ww)
+let We = Ww-Wq;
+console.log(We);
+W2.style.width = We + "px";
+console.log('Привет');
+console.log(Wq);
+console.log(W2.style.width);
+console.log(W11.width);
+
