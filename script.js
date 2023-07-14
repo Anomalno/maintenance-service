4ba6cca8f2e7f3fc781e193000db8687d3ccb615
Anomalno <treskov.andrey@mail.ru>
2023-07-14 14:04:19 +0500

    1

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
-let W1 = document.querySelector(".nav-block-button");
-let W2 = document.querySelector(".nav-block-2");
-let W11 = getComputedStyle(W1);
-let W22 = getComputedStyle(W2);
-let Wq = parseFloat(W22.paddingLeft);
-console.log(Wq)
-let Ww = parseFloat(W11.width);
-console.log(Ww)
-let We = Ww-Wq;
-console.log(We);
-W2.style.width = We + "px";
-console.log('Привет');
-console.log(Wq);
-console.log(W2.style.width);
-console.log(W11.width);
+let i = 1;
+let n = 2;
+let count_of_main_elements = document.querySelectorAll("li.nav-block-button").length;
+if (window.matchMedia('screen and (min-width: 320px) and (max-width: 576px)').matches) {
+
+}
+else {
+    while (i <= count_of_main_elements) {
+        let main_element = "li.nav-block-button:nth-of-type" + "(" + i * n + ")";
+        let equ_element = main_element + " > ul.nav-block-2";
+        let count_of_main_elements = document.querySelectorAll("li.nav-block-button").length;
+        console.log("Число мейн элементов = ", count_of_main_elements);
+        console.log("main_element = ", document.querySelector(main_element));
+        console.log("equ_element = ", document.querySelector(equ_element));
+        document.querySelector(equ_element).style.width = getComputedStyle(document.querySelector(main_element)).width;
+        console.log(document.querySelector("li.nav-block-button:nth-of-type" + "(" + 2 * i + ")"));
+        i++;
+    }
+}
 
